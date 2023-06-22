e484db0680ec8d5b902bcfa2ec05058ea34d124b
Leynardo Yosef <leynardo.yosef@logkar.com>
2023-06-22 15:43:05 +0700

    ➕feat(app): Fix Update order

--- Controller/Dto/request/Order.go
+++ Controller/Dto/request/Order.go
@@ -17,6 +17,6 @@ type RateOrder struct {
 type UpdateOrder struct {
 	OrderId   int `json:"order_id" field:"required" min:"1"`
 	UserId    int `json:"user_id" field:"required" min:"1"`
-	NewStatus int `json:"new_status" field:"required" min:"1"`
+	NewStatus int `json:"new_status" field:"required" min:"0"`
 	TakenBy   int `json:"taken_by" field:"required" min:"1"`
 }
